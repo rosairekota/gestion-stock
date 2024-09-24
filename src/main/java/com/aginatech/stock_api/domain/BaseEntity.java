839fc0cfa4ad91f99bbb72abc6ea0425992728ba
rkota <rosairekota@anicns.gouv.cd>
2024-09-24 02:51:29 +0100

    refactor: add EntityListeners

--- src/main/java/com/aginatech/stock_api/domain/BaseEntity.java
+++ src/main/java/com/aginatech/stock_api/domain/BaseEntity.java
@@ -1,10 +1,11 @@
 package com.aginatech.stock_api.domain;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -13,15 +14,24 @@ import java.util.Date;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+/*AuditingEntityListener: Permet d'assigner automatiquement
+des valeurs createAt et updatedAt une fois que l'entite est mise a jour
+*/
 public class BaseEntity implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @CreatedDate
+    @JsonIgnore // permet d'ignorer la proprieter lorsqu'on get l'entite[GET]
+    @Column(name="create_at")
     private Instant createAt;
 
 
     @LastModifiedDate
+    @JsonIgnore
+    @Column(name = "updated_at")
     private  Instant updatedAt;
 }
